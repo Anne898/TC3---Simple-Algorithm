69c7f8bcb0ad81548afb6fa594d753a608921f36
Anne898 <45019588+Anne898@users.noreply.github.com>
2020-09-18 14:01:36 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Algoritmo_Numeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        int numero;
+
+        Console.WriteLine("Escribe un numero");
+
+        numero = Convert.ToInt32(Console.ReadLine());
+
+        System.Console.Write(numero);
+
+        for (int i = 1; i <= numero; i++)
+        {
+
+            System.Console.Write("," + i);
+
+        }
+
+
+        }
+    }
+}
